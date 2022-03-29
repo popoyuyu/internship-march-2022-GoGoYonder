91a965c846435b8fc12cdeaf1ee40fedb7e811aa
Jeremy Banka <hello@jeremybanka.com>
2022-03-28 18:35:43 -0700

    🔧 use company style guide

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,10 +3,11 @@
  */
 module.exports = {
   extends: [
-    "@remix-run/eslint-config",
-    "@remix-run/eslint-config/node",
-    "@remix-run/eslint-config/jest",
-    "prettier",
+    `@eyecuelab/react`,
+    // "@remix-run/eslint-config",
+    // "@remix-run/eslint-config/node",
+    // "@remix-run/eslint-config/jest",
+    // "prettier",
   ],
   // we're using vitest which has a very similar API to jest
   // (so the linting plugins work nicely), but it we have to explicitly
@@ -16,4 +17,7 @@ module.exports = {
       version: 27,
     },
   },
-};
+  parserOptions: {
+    project: [`./tsconfig.json`, `./cypress/tsconfig.json`],
+  },
+}
