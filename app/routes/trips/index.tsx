de7f21c8643d5bb1e297d03fb6efdbf9314d2593
Jacob Wilson <jacobleeeugenewilson@gmail.com>
2022-04-19 15:02:03 -0800

    adjust loader data, WIP styling

--- app/routes/trips/index.tsx
+++ app/routes/trips/index.tsx
@@ -1,45 +1,51 @@
 import type { FC } from "react"
 
 import { Link, json, useLoaderData, LoaderFunction } from "remix"
-
-import type { Trip, Attendee } from "@prisma/client"
+import { Params } from "react-router-dom"
+import invariant from "tiny-invariant"
+import type { Trip, Attendee, User } from "@prisma/client"
 import { getAttendeesByUserId } from "~/models/attendee.server"
 import { getTripById } from "~/models/trip.server"
 import { requireUserId } from "~/session.server"
 import { join } from "~/utils"
+import { TripLiContainer, TripLiImage, TripLiTitle, TripLiFlex, TripLiDetail, TripLiGroup } from "../../styles/styledComponents"
 
-type LoaderData = {
-  pendingTrips: Trip[];
-  acceptedTrips: Trip[];
-}
+type LoaderData = Awaited<ReturnType<typeof getLoaderData>>;
 
-export const loader: LoaderFunction = async ({ request }) => {
+const getLoaderData = async (request: Request, params: Params<string>) => {
   const userId = await requireUserId(request)
+  invariant(userId, `userId required`)
   const attendees = await getAttendeesByUserId(userId)
   const pending = attendees.filter(trip => trip.isAccepted === null)
   const accepted = attendees.filter(trip => trip.isAccepted !== null)
-  const pendingPromises = await Promise.all(
+  const pendingArray = await Promise.all(
     pending.map(async (attendee) => (
       await getTripById(attendee.tripId)
     )))
-  const acceptedPromises = await Promise.all(
+  const acceptedArray = await Promise.all(
     accepted.map(async (attendee) => (
       await getTripById(attendee.tripId)
     )))
 
-  const pendingTrips = pendingPromises as Trip[]
-  const acceptedTrips = acceptedPromises as Trip[]
+  return {
+    trips: {
+      accepted: acceptedArray,
+      pending: pendingArray
+    }
+  }
+}
 
-  console.log(pendingTrips)
-  console.log(acceptedTrips)
-  return json<LoaderData>({
-    pendingTrips: pendingTrips,
-    acceptedTrips: acceptedTrips
-  })
+export const loader: LoaderFunction = async ({
+  request,
+  params
+}) => {
+
+  return json<LoaderData>(await getLoaderData(request, params))
 }
 
 const Index: FC = () => {
-  const { pendingTrips, acceptedTrips } = useLoaderData() as LoaderData
+  const data = useLoaderData<LoaderData>()
+
   const linkStyles = [
     `flex`,
     `items-center`,
@@ -63,30 +69,44 @@ const Index: FC = () => {
         Pending Trips
       </h1>
       <ul>
-        {pendingTrips.map((trip: Trip) => (
-          <li key={trip.id}>
-            <Link
-              to={trip.id}
-              className={join(...linkStyles)}
-            >
-              {trip.nickName}
-            </Link>
-          </li>
+        {data.trips.pending.map((trip) => (
+          <TripLiContainer>
+            <TripLiImage src="https://images.unsplash.com/photo-1541570213932-8cd806e3f8f6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTF8fHJvYWQlMjB0cmlwfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=400&q=60" />
+            <TripLiTitle>{trip?.stops[0] ? trip?.stops[0] : "Start"} to {trip?.stops[-1] ? trip?.stops[-1] : "End"}</TripLiTitle>
+            <hr />
+            <TripLiFlex>
+              <TripLiGroup>Starts</TripLiGroup>
+              <TripLiGroup>Ends</TripLiGroup>
+              <TripLiGroup>Stops</TripLiGroup>
+            </TripLiFlex>
+            <TripLiFlex>
+              <TripLiDetail>{trip?.startDate}</TripLiDetail>
+              <TripLiDetail>{trip?.endDate}</TripLiDetail>
+              <TripLiDetail>{trip?.stops.length}</TripLiDetail>
+            </TripLiFlex>
+          </TripLiContainer>
         ))}
       </ul>
       <h1 className={join(`flex`, `items-center`, `justify-center`)}>
         Accepted Trips
       </h1>
       <ul>
-        {acceptedTrips.map((trip: Trip) => (
-          <li key={trip.id}>
-            <Link
-              to={trip.id}
-              className={join(...linkStyles)}
-            >
-              {trip.nickName}
-            </Link>
-          </li>
+        {data.trips.accepted.map((trip) => (
+          <TripLiContainer>
+            <TripLiImage src="https://images.unsplash.com/photo-1541570213932-8cd806e3f8f6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTF8fHJvYWQlMjB0cmlwfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=400&q=60" />
+            <TripLiTitle>{trip?.stops[0]} to {trip?.stops[-1]}</TripLiTitle>
+            <hr />
+            <TripLiFlex>
+              <TripLiGroup>Starts</TripLiGroup>
+              <TripLiGroup>Ends</TripLiGroup>
+              <TripLiGroup>Stops</TripLiGroup>
+            </TripLiFlex>
+            <TripLiFlex>
+              <TripLiDetail>{trip?.startDate}</TripLiDetail>
+              <TripLiDetail>{trip?.endDate}</TripLiDetail>
+              <TripLiDetail>{trip?.stops.length}</TripLiDetail>
+            </TripLiFlex>
+          </TripLiContainer>
         ))}
       </ul>
       <Link
@@ -106,4 +126,3 @@ const Index: FC = () => {
 }
 
 export default Index
-//outlet
