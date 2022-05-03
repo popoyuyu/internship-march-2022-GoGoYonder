748da9a08cd6354a87b811b20c230107caef974b
Ella Tanttu <88292299+ellajtanttu@users.noreply.github.com>
2022-05-03 11:36:55 -0700

    add static content to profile page

--- app/routes/profile/index.tsx
+++ app/routes/profile/index.tsx
@@ -2,15 +2,31 @@ import type { FC } from "react"
 
 import { Link, useLoaderData, Form } from "remix"
 
+import { Header, SubHeader } from "~/styles/styledComponents"
+import SvgBackButton from "~/styles/SVGR/SvgBackButton"
 import { join } from "~/utils"
 
 import NavBar from "../navbar"
 
 const Index: FC = () => {
   return (
-    <div>
+    <>
+      <div className={join(`ml-8`)}>
+        <SvgBackButton />
+      </div>
+      <Header>Your Account</Header>
+      <div>
+        <p>Total Trips:</p>
+        <p>Member Since:</p>
+      </div>
+      <div>
+        <SubHeader>Recent Trips</SubHeader>
+      </div>
+      <ul>
+        <li>Placeholder Trip 1</li>
+      </ul>
       <NavBar />
-    </div>
+    </>
   )
 }
 
