bd846f22cb83bbac08e9ea8df38b21b27123fffd
Jacob Wilson <77640888+JLEWilson@users.noreply.github.com>
2022-05-10 11:24:15 -0800

    update date display (#52)

--- app/components/TripView.tsx
+++ app/components/TripView.tsx
@@ -42,9 +42,11 @@ const LinkComponents: FC<TripViewProps> = ({ trip }) => {
         <TripLiGroup>Ends</TripLiGroup>
         <TripLiGroup>Stops</TripLiGroup>
         <TripLiDetail>
-          {trip.startDate ? trip.startDate : `00/00/00`}
+          {trip.startDate ? trip.startDate.toLocaleDateString() : `00/00/00`}
         </TripLiDetail>
-        <TripLiDetail>{trip.endDate ? trip.endDate : `00/00/00`}</TripLiDetail>
+        <TripLiDetail>
+          {trip.endDate ? trip.endDate.toLocaleDateString() : `00/00/00`}
+        </TripLiDetail>
         <TripLiDetail>{trip.stops.length}</TripLiDetail>
       </TripLiFlex>
     </>
@@ -78,7 +80,6 @@ const TripView: FC<TripViewProps> = ({ trip, isAccepted = true }) => {
               </Form>
             </>
           )}
-
         </TripLiContainer>
       </ul>
     </div>
